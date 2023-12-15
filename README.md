59a95a3dfe4849dd85b4458af45fb4f53cc8ede5
alunosNET <alunosnet@gmail.com>
2023-12-15 15:38:04 +0000

    Enunciado do desafio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # M03_Desafio_00
+Este é o desafio 0 do módulo 3.
+Cria um branch com o teu nº processo
+Resolve os exercícios que estão enunciados nos comentários
+Submete o teu branch no final
---
+++ desafio0.py
@@ -0,0 +1,13 @@
+"""Cria uma função que recebe um texto. A função deve mostrar uma letra sempre que existirem duas letras seguidas iguais.
+Por exemplo: 
+    Hello - deve mostrar l
+    """
+
+
+"""Cria uma função que recebe dois números. Esta função deve mostrar quantos dos números são pares."""
+
+
+
+"""Faz uma função que recebe um texto e devolve o texto invertido."""
+
+"""Faz uma função que recebe """
