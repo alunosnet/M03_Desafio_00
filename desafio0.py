564d0650dc5b2c97153455770c9d6b92a5ec1a72
alunosNET <alunosnet@gmail.com>
2023-12-15 16:49:16 +0000

    Pequenas correções

--- desafio0.py
+++ desafio0.py
@@ -1,13 +1,21 @@
-"""Cria uma função que recebe um texto. A função deve mostrar uma letra sempre que existirem duas letras seguidas iguais.
+"""
+1 - Cria uma função que recebe um texto. A função deve mostrar uma letra sempre que existirem duas letras seguidas iguais.
 Por exemplo: 
     Hello - deve mostrar l
-    """
+"""
 
 
-"""Cria uma função que recebe dois números. Esta função deve mostrar quantos dos números são pares."""
+"""
+2 - Cria uma função que recebe dois números. Esta função deve mostrar quantos dos números são pares.
+"""
 
 
 
-"""Faz uma função que recebe um texto e devolve o texto invertido."""
+"""
+3 - Faz uma função que recebe um texto e devolve o texto invertido.
+"""
 
-"""Faz uma função que recebe """
+
+"""
+4 - Faz uma função que recebe 
+"""
