6debdce571111f4720a5eff8cae50440f2c62a89
Afonsomsloureiro <afonsomsloureiro@gmail.com>
2024-01-03 12:01:09 +0000

    ex1

--- desafio0.py
+++ desafio0.py
@@ -4,6 +4,22 @@ Por exemplo:
     Hello - deve mostrar l
 """
 
+def letra():
+    palavra=input("palavra:")
+    palavra=palavra.lower()
+    for posiçao in range(len(palavra)-1):
+        if palavra[posiçao]==palavra[posiçao+1]:
+            print(palavra[posiçao])
+
+letra()
+def letra2():
+    palavra=input("palavra:")
+    posicao=0
+    while posicao< len(palavra)-1:
+        if palavra[posicao]==palavra[posicao+1]:
+            print(palavra[posicao])
+        posicao=posicao+1
+letra2()
 
 """
 2 - Cria uma função que recebe três números. Esta função deve mostrar, para os números passados, quantos são pares.
@@ -14,6 +30,12 @@ Por exemplo:
 """
 3 - Faz uma função que recebe um texto e devolve o texto invertido.
 """
+def invertida():
+    palavra=input("palavra:")
+    palavra=palavra.lower()
+    for letra in palavra:
+        pass
+
 
 
 """
