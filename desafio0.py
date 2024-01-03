3ebf3eb1f8480c00c517dcd0884f391f53cd8734
Kr1z13 <rafael13kiko@gmail.com>
2024-01-03 12:00:59 +0000

    ex1

--- desafio0.py
+++ desafio0.py
@@ -10,6 +10,15 @@ def LetraRepetida(palavra):
 
 LetraRepetida("Hello")
 
+def LetraRepetidaV2(palavra):
+    posicao = 0
+    while posicao < len(palavra) - 1:
+        if palavra[posicao] == palavra[posicao + 1]:
+            print(palavra[posicao])
+        posicao = posicao + 1
+
+LetraRepetidaV2("Hello")
+
 """
 2 - Cria uma função que recebe três números. Esta função deve mostrar, para os números passados, quantos são pares.
 """
@@ -25,11 +34,11 @@ def NumerosPares(n1,n2,n3):
 
 NumerosPares(1,2,4)
 
-
-
 """
 3 - Faz uma função que recebe um texto e devolve o texto invertido.
 """
+def TextoInvertido(palavra):
+    pass
 
 
 """
@@ -37,3 +46,11 @@ NumerosPares(1,2,4)
 Por exemplo:
     Hello - deve devolver Hlo
 """
+
+def TextoAlternado(palavra):
+    palavra_alternada = ""
+    for posicao in range(len(palavra)):
+        palavra_alternada = palavra_alternada + palavra[posicao + 2]
+    print(palavra_alternada)
+
+TextoAlternado("Hello")
