6a2f97d50bcf50bb82692477b867a0d8c376288d
DanPeregrineDev <madeira.daf@gmail.com>
2024-01-03 11:44:32 +0000

    2 exercicio

--- desafio0.py
+++ desafio0.py
@@ -6,20 +6,30 @@ Por exemplo:
 
 def duplicatedLetters(word):
     letter = ""
-    text = ""
+    final = ""
     
     for i in word:
         if i == letter:
-            text = text + letter
+            final = final + letter
         letter = i
 
-    print("Duplicated letters: ", text)
+    print("Duplicated letters: ", final)
 
 """
 2 - Cria uma função que recebe três números. Esta função deve mostrar, para os números passados, quantos são pares.
 """
 
+def howManyPairNumbers(n1, n2, n3):
+    howMany = 0
 
+    if n1 % 2 == 0:
+        howMany = howMany + 1
+    if n2 % 2 == 0:
+        howMany = howMany + 1
+    if n3 % 3 == 0:
+        howMany = howMany + 1
+
+    print("Ammount of pair numbers: ", howMany)
 
 """
 3 - Faz uma função que recebe um texto e devolve o texto invertido.
