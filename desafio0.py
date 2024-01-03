8a3f2194cd855c50841299d8c5fd3a01279ef609
DFBbrito <a24567@esenviseu.net>
2024-01-03 12:42:00 +0000

    exercicio 3

--- desafio0.py
+++ desafio0.py
@@ -9,25 +9,27 @@ def LetrasIguais(palavra):
         if palavra[posicao]==palavra[posicao+1]:
             print(palavra[posicao])
 
-LetrasIguais('Hello')
-print('-----')
-
 """
 2 - Cria uma função que recebe três números. Esta função deve mostrar, para os números passados, quantos são pares.
 """
 def Pares(n1,n2,n3):
+    contar=0
     if n1%2==0:
-        print('par')
+        contar=contar+1
     if n2%2==0:
-        print('par')
+        contar=contar+1
     if n3%2==0:
-        print('par')
-
-Pares(6,20,9)
+        contar=contar+1
+    print(contar)
 
 """
 3 - Faz uma função que recebe um texto e devolve o texto invertido.
 """
+def InverterTexto(palavra):
+    texto_invertido=""
+    for letra in palavra:
+        texto_invertido=
+    return texto_invertido
 
 
 """
@@ -35,3 +37,12 @@ Pares(6,20,9)
 Por exemplo:
     Hello - deve devolver Hlo
 """
+
+
+def main():
+    LetrasIguais("Hello")
+    Pares(10,4,8)
+    Pares(23,6,3)
+
+if __name__ == "__main__":
+    main()
