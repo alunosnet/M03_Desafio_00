d0d52eff4821108c1100ba4b7625be7c138c5fb4
Fredoono <mimikyopokemon@gmail.com>
2024-01-03 12:02:35 +0000

    termineir o exercicio 1

--- desafio0.py
+++ desafio0.py
@@ -12,18 +12,39 @@ def NumeroLetrasSeguidos(palav):
         letra1 = i
     print (texto)
 
-NumeroLetrasSeguidos("Helllo")
+""" versão dois do exercício um"""
+
+def NumeroLetrasSeguidosVersao2(palav):
+    numer1 = 0
+    texto = ""
+    while True:
+        if palav[numer1] == palav[numer1+1]:
+            texto = texto + palav[numer1]
+        if len(palav)-1 > numer1:
+            break
+        numer1 += 1
+    print (texto)
+
+
+
+NumeroLetrasSeguidosVersao2("hello")
+
+NumeroLetrasSeguidos("Feederico")
 
 
 """
 2 - Cria uma função que recebe três números. Esta função deve mostrar, para os números passados, quantos são pares.
 """
-
+def TresPares():
+    exit
 
 
 """
 3 - Faz uma função que recebe um texto e devolve o texto invertido.
 """
+def TextoInvertido():
+    
+    exit
 
 
 """
